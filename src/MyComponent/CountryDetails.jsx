27b8e4e35efd9443adb458f353cfff04282ce0db
Kuldeeep Choudhary <kulchoudhary21@gmil.com>
2023-03-20 18:08:33 +0530

    Adding some code and any make Country drop down list

--- src/MyComponent/CountryDetails.jsx
+++ src/MyComponent/CountryDetails.jsx
@@ -1,37 +1,36 @@
 import { useState } from "react";
 import "../css/country.css";
+let count = true;
 export function CountryDetails() {
-  const [c,getC]=useState({})
-  
-//   const getCity=()=>{
-//     const countries={
-//         india:["Indore,ujjain,Dewas,mumbai"],
-//         australia:["melbourne,sydney"],
-//         newzealand:["1","2"]
-//       };
-//       let v=document.getElementById().value;
-//   }  
-  let getCt=(e)=>{
-      let countries = [
-        {country: 'india', cities: ['Indore','ujjain','Dewas','mumbai']},
-        {country: 'australia', cities: ['Indore','ujjain','Dewas','mumbai']},
-        {country: 'pakistan', cities: ['Islamabad','lahore','karachi']},
-      ]
-    
-      countries.map((list)=>{
-        console.log(list);
-      })
+  const [country, setCountry] = useState(["Select Option"]);
+  let countries = [
+    { country: "india", cities: ["Indore", "ujjain", "Dewas", "mumbai"] },
+    { country: "australia", cities: ["Indore", "ujjain", "Dewas", "mumbai"] },
+    { country: "pakistan", cities: ["Islamabad", "lahore", "karachi"] },
+  ];
+  let getCt = () => {
+    const select = document.getElementById("select").value;
+    console.log(count);
+    countries.map((l, i) => {
+      if (count) {
+        setCountry((arr) => [...arr, l.country]);
+      }
+      if (countries.length == i + 1) count = false;
 
-  }
+      if (l.country == select) {
+        console.log(countries[i].cities);
+      }
+    });
+  };
+
   return (
     <div className="country">
-      <label  style={{padding:"10px"}}>Choose a Country:</label>
+      <label style={{ padding: "10px" }}>Choose a Country:</label>
       <div>
-      
-        <select name="Country">
-          <option value="india" onChange={getCt}>India</option>
-          <option value="australia" onChange={getCt}>Australia</option>
-          <option value="newzealand" onChange={getCt} name="newzealand">New Zealand</option>
+        <select id="select" name="d1" onClick={getCt}>
+          {country.map((i) => {
+            return <option value={i}>{i}</option>;
+          })}
         </select>
       </div>
     </div>
