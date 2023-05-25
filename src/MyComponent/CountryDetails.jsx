ca5f5bdb50a5b8942a24f435a703d595f8d162d4
Kuldeeep Choudhary <kulchoudhary21@gmil.com>
2023-05-25 20:02:41 +0530

    Adding src

--- src/MyComponent/CountryDetails.jsx
+++ src/MyComponent/CountryDetails.jsx
@@ -2,19 +2,18 @@ import { useState } from "react";
 import "../css/country.css";
 import { countries } from "./daa";
 export function CountryDetails() {
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState([]);
 
   const selectCountry = (e) => {
-// console.log(countries[e.target.value].cities)
-    setCity(countries[e.target.value].cities)
-  }
+    setCity(countries[e.target.value].cities);
+  };
 
   return (
     <div className="country">
       <label style={{ padding: "10px" }}>Choose a Country:</label>
       <div>
         <select id="select" onChange={selectCountry}>
-          {countries.map((i,index) => {
+          {countries.map((i, index) => {
             return (
               <option name={i.country} key={i.country} value={index}>
                 {i.country}
@@ -23,12 +22,12 @@ export function CountryDetails() {
           })}
         </select>
 
-        <select id="select"  >
-            return (
-              <option key="k">
-                {city}
-              </option>
-            );
+        <select id="select">
+          {city.map((list)=>{
+          return(
+          <option key="k">{list}</option>
+          );
+        })}
         </select>
       </div>
     </div>
